f03711eca260951ad71300d68f93379fa410f2a2
Marcos Paulo <marcospaulosousa48@gmail.com>
2026-02-08 12:48:17 -0300

    update json server

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server rodando na porta ${PORT}`);
 });
